cf490e2aafe211a5bc5c1d2aac23b75307d9b90f
kintsoogi <kintsoogii@proton.me>
2024-01-31 12:59:15 -0600

    Add translation note view for one note

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,6 +1,33 @@
 // import { vscode } from "./utilities/vscode";
 import { VSCodePanels, VSCodePanelTab, VSCodePanelView } from "@vscode/webview-ui-toolkit/react";
 import "./App.css";
+import { markdownToHTML } from "scripture-tsv";
+import type { ScriptureTSV } from "scripture-tsv";
+
+const TITUS_1_1_TSV: ScriptureTSV = {
+  1: {
+    1: [
+      {
+        Reference: "1:1",
+        ID: "h5gl",
+        Tags: "",
+        SupportReference: "rc://*/ta/man/translate/figs-exclusive",
+        Quote: "",
+        Occurrence: "0",
+        Note: "Though this letter is from Paul and Timothy to the Colossian believers, later in the letter Paul makes it clear that he is the writer. Most likely Timothy was with him and wrote the words down as Paul spoke. Throughout this letter the words “we,” “our,” and “ours” include the Colossians unless noted otherwise. The words “you,” “your,” and “yours” refer to the Colossian believers and so are plural unless noted otherwise. (See: [[rc://*/ta/man/translate/figs-exclusive]] and [[rc://*/ta/man/translate/figs-you]])",
+      },
+      {
+        Reference: "1:1",
+        ID: "fny3",
+        Tags: "",
+        SupportReference: "",
+        Quote: "ἀπόστολος Χριστοῦ Ἰησοῦ διὰ θελήματος Θεοῦ",
+        Occurrence: "1",
+        Note: "“whom God chose to be an apostle of Christ Jesus”",
+      },
+    ],
+  },
+};
 
 function App() {
   // TODO: Implement note navigation
@@ -10,6 +37,12 @@ function App() {
   //     text: "Navigating verse notes",
   //   });
   // }
+
+  const origWordHeading = !!TITUS_1_1_TSV[1][1][0].Quote
+    ? TITUS_1_1_TSV.Quote
+    : "General Verse Note";
+
+  const noteHTML = markdownToHTML(TITUS_1_1_TSV[1][1][0].Note);
 
   return (
     <main>
@@ -21,7 +54,8 @@ function App() {
           {/* <VSCodePanelView id="view-book">Problems content.</VSCodePanelView> */}
           {/* <VSCodePanelView id="view-chapter">Output content.</VSCodePanelView> */}
           <VSCodePanelView id="view-verse">
-            Coming soon to you... translation notes details
+            <h3>{origWordHeading}</h3>
+            <p>{noteHTML}</p>
           </VSCodePanelView>
         </VSCodePanels>
       </section>
