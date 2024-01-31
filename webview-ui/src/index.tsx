0a6e6f1c131d92439beb0d8197ce7e8617c78018
kintsoogi <kintsoogii@proton.me>
2024-01-31 12:59:03 -0600

    Update dependencies

--- webview-ui/src/index.tsx
+++ webview-ui/src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
+// Assuming the element with id 'root' exists in your HTML file
+const rootElement = document.getElementById("root") as HTMLElement;
+
+// Create a root.
+const root = ReactDOM.createRoot(rootElement);
+
+// Initial render: Render the App component to the root.
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
