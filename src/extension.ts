e0f73e76332390b24ef92a7405e7f2c72a5a58d2
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2024-07-24 09:17:35 -0400

    restore file content clobbered by `yo`

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { CheckingProvider } from "./CheckingProvider";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,16 +11,13 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "checking-extension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('checking-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Loaded checking-extension!');
-	});
-
-	context.subscriptions.push(disposable);
+	vscode.window.showInformationMessage('Loaded checking-extension!');
+	
+	// Register the custom checker provider
+	const { providerRegistration, commandRegistration } =
+		CheckingProvider.register(context);
+	context.subscriptions.push(providerRegistration);
+	context.subscriptions.push(commandRegistration);
 }
 
 // This method is called when your extension is deactivated
