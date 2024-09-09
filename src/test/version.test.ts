35fa135a862eb5ebc6760d654398c82f868a8d69
PhotoNomad0 <bruce.mclean@unfoldingword.org>
2024-09-09 07:44:01 -0400

    remove unsupported menu items

---
+++ src/test/version.test.ts
@@ -0,0 +1,25 @@
+// @ts-ignore
+import * as fs from "fs-extra";
+import * as assert from 'assert';
+
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+// import * as myExtension from '../extension';
+
+suite('Check Versions', () => {
+  suiteTeardown(() => {
+    vscode.window.showInformationMessage('Version tests done!');
+  });
+
+  test('Compare extension and webview-UI versions', () => {
+    const extensionManifest = fs.readJsonSync('./package.json');
+    const webviewUiManifest = fs.readJsonSync('./webview-ui/package.json');
+
+    const extensionVersion = extensionManifest?.version;
+    assert.ok(extensionVersion)
+    const webviewUinVersion = webviewUiManifest?.version;
+    assert.ok(webviewUinVersion)
+    assert.equal(extensionVersion, webviewUinVersion)
+  });
+});
